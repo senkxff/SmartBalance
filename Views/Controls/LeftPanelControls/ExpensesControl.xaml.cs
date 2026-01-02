c70ec4a1a2d7ff3a0142ad6ab2605a5314171946
senkxff <senkxff@gmail.com>
2026-01-02 18:24:36 +0300

    Create ExpensesControl (without logic)

---
+++ Views/Controls/LeftPanelControls/ExpensesControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SmartBalance.Views.Controls.LeftPanelControls
+{
+    /// <summary>
+    /// Логика взаимодействия для ExpensesControl.xaml
+    /// </summary>
+    public partial class ExpensesControl : UserControl
+    {
+        public ExpensesControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
