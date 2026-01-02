79867fafbaeb5d446269bcab296141390f54d428
senkxff <senkxff@gmail.com>
2026-01-02 23:21:24 +0300

    Create SalaryControl (without logic)

---
+++ Views/Controls/PagesControls/IncomePageControls/SalaryControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SmartBalance.Views.Controls.PagesControls.IncomePageControls
+{
+    /// <summary>
+    /// Логика взаимодействия для SalaryControl.xaml
+    /// </summary>
+    public partial class SalaryControl : UserControl
+    {
+        public SalaryControl()
+        {
+            InitializeComponent();
+        }
+
+        // Изменение при наведении
+        private void Grid_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Ellipse.Fill = new SolidColorBrush(Colors.DarkOrange);
+            Title.Foreground = Brushes.DarkSlateGray;
+            Sum.Foreground = Brushes.Green;
+        }
+        private void Grid_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Ellipse.Fill = new SolidColorBrush(Colors.Orange);
+            Title.Foreground = Brushes.Gray;
+            Sum.Foreground = Brushes.ForestGreen;
+        }
+    }
+}
