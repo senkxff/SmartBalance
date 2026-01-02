d4cbf6fe1ecd70403568891d1e34bf45fde1d630
senkxff <senkxff@gmail.com>
2026-01-02 19:29:57 +0300

    Create StoryControl (without logic)

---
+++ Views/Controls/LeftPanelControls/StoryControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SmartBalance.Views.Controls.LeftPanelControls
+{
+    /// <summary>
+    /// Логика взаимодействия для StoryControl.xaml
+    /// </summary>
+    public partial class StoryControl : UserControl
+    {
+        public StoryControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
