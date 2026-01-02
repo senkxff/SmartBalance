1cf20ac0b732b03e5e237e961317f9db24556416
senkxff <senkxff@gmail.com>
2026-01-02 18:48:03 +0300

    Create WalletsControl (without logic) and update LeftPanel

---
+++ Views/Controls/LeftPanelControls/WalletsControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SmartBalance.Views.Controls.LeftPanelControls
+{
+    /// <summary>
+    /// Логика взаимодействия для WalletsControl.xaml
+    /// </summary>
+    public partial class WalletsControl : UserControl
+    {
+        public WalletsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
