f202f40a29ab3c6e85aa983a8448b8437f761cba
senkxff <senkxff@gmail.com>
2026-01-02 19:01:55 +0300

    Update LeftPanel, create AnalyticControl (without logic), update IncomeControl, update ClosingWarningWindow

---
+++ Views/Controls/LeftPanelControls/AnalyticControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SmartBalance.Views.Controls.LeftPanelControls
+{
+    /// <summary>
+    /// Логика взаимодействия для AnalyticControl.xaml
+    /// </summary>
+    public partial class AnalyticControl : UserControl
+    {
+        public AnalyticControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
