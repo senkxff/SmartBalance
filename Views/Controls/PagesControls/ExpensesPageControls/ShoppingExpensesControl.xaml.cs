18fe48a9797d8218782c875e3d8ec2916560449d
senkxff <senkxff@gmail.com>
2026-01-04 00:55:29 +0300

    Create ShoppingExpensesControl

--- Views/Controls/PagesControls/ExpensesPageControls/ShoppingExpensesControl.xaml.cs
+++ Views/Controls/PagesControls/ExpensesPageControls/ShoppingExpensesControl.xaml.cs
@@ -1,38 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Shapes;
 
 namespace SmartBalance.Views.Controls.PagesControls.ExpensesPageControls
 {
     /// <summary>
-    /// Логика взаимодействия для TransportExpensesControl.xaml
+    /// Логика взаимодействия для ShoppingExpensesControl.xaml
     /// </summary>
-    public partial class TransportExpensesControl : UserControl
+    public partial class ShoppingExpensesControl : UserControl
     {
-        public TransportExpensesControl()
+        public ShoppingExpensesControl()
         {
             InitializeComponent();
         }
 
         private void Grid_MouseEnter(object sender, MouseEventArgs e)
         {
-            Ellipse.Fill = new SolidColorBrush(Colors.Orange);
+            Ellipse.Fill = new SolidColorBrush(Colors.YellowGreen);
             Title.Foreground = Brushes.DarkSlateGray;
             Sum.Foreground = Brushes.Red;
         }
 
         private void Grid_MouseLeave(object sender, MouseEventArgs e)
         {
-            Ellipse.Fill = new SolidColorBrush(Colors.Yellow);
+            Ellipse.Fill = new SolidColorBrush(Colors.GreenYellow);
             Title.Foreground = Brushes.Gray;
             Sum.Foreground = Brushes.IndianRed;
         }
