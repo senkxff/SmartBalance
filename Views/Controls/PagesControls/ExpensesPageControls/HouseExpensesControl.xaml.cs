8cd49681de7a0973227003d4f419a5d644e268b6
senkxff <senkxff@gmail.com>
2026-01-03 23:56:31 +0300

    Create HouseExpensesControl (without logic)

---
+++ Views/Controls/PagesControls/ExpensesPageControls/HouseExpensesControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SmartBalance.Views.Controls.PagesControls.ExpensesPageControls
+{
+    /// <summary>
+    /// Логика взаимодействия для HouseExpensesControl.xaml
+    /// </summary>
+    public partial class HouseExpensesControl : UserControl
+    {
+        public HouseExpensesControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Ellipse.Fill = new SolidColorBrush(Colors.Purple);
+            Title.Foreground = Brushes.DarkSlateGray;
+            Sum.Foreground = Brushes.Red;
+        }
+
+        private void Grid_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Ellipse.Fill = new SolidColorBrush(Colors.MediumPurple);
+            Title.Foreground = Brushes.Gray;
+            Sum.Foreground = Brushes.IndianRed;
+        }
+    }
+}
