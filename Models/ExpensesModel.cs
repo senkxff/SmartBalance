8d15dc43e66da270dc844ea7abac45dd92e06e5c
senkxff <senkxff@gmail.com>
2026-01-13 15:36:07 +0300

    Start creating of MVVM bindings

--- Models/ExpensesModel.cs
+++ Models/ExpensesModel.cs
@@ -3,7 +3,7 @@
     class ExpensesModel
     {
         public string? category { get; set; }
-        public decimal amount { get; set; }
+        public decimal amount { get; set; } = 0;
         public DateTime date { get; set; }
         public string? wallet { get; set; }
         public string? comment { get; set; }
